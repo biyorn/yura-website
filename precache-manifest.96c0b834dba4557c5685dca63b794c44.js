435a249774866b995dcd19651d01c5f3ca67ba19
Pavel <porlovski3@gmail.com>
2020-07-09 20:50:25 +0300

    Updates

---
+++ precache-manifest.96c0b834dba4557c5685dca63b794c44.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bcb9564bdcf83f2313d32b14032948a",
+    "url": "/yura-website/index.html"
+  },
+  {
+    "revision": "5b7646f30bf20b7ed376",
+    "url": "/yura-website/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a028bc1e2d4ee8663fcd",
+    "url": "/yura-website/static/js/2.32383106.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/yura-website/static/js/2.32383106.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b7646f30bf20b7ed376",
+    "url": "/yura-website/static/js/main.11505484.chunk.js"
+  },
+  {
+    "revision": "b65ed3c11cf81f6f8000",
+    "url": "/yura-website/static/js/runtime-main.29cd565a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/yura-website/static/media/logo.5d5d9eef.svg"
+  }
+]);
